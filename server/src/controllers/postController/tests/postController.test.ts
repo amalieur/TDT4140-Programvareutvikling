dadaa59b245b06b59bd28145a79a617e57906bda
shirajuki <shirajuki00@gmail.com>
2021-02-18 00:16:37 +0100

    test: add test for category filtering on post (#6)

--- server/src/controllers/postController/tests/postController.test.ts
+++ server/src/controllers/postController/tests/postController.test.ts
@@ -1,28 +1,35 @@
-import request from 'supertest';
-import app from '../../../app';
+import request from "supertest";
+import app from "../../../app";
 
-describe('Test postController', () => {
-    beforeAll(async () => { // kjører før testing
-        console.log("Post controller test starting...");
-    });
+describe("Test postController", () => {
+  beforeAll(async () => {
+    // kjører før testing
+    console.log("Post controller test starting...");
+  });
 
-    afterAll(async () => { // kjører når all testing er gått gjennom
-        console.log("...Test ending");
-    });
+  afterAll(async () => {
+    // kjører når all testing er gått gjennom
+    console.log("...Test ending");
+  });
 
-    it('Request /api/post should return request of 200!', async () => {
-        const result = await request(app)
-        .get('/api/post')
-        .send()
+  it("Request /api/post should return request of 200!", async () => {
+    const result = await request(app).get("/api/post").send();
 
-        expect(result.status).toBe(200);
-    });
+    expect(result.status).toBe(200);
+  });
 
-    it('Request /api/post/1 should return data with name "test"!', async () => {
-        const result = await request(app).get('/api/post/1').send();
+  it('Request /api/post/1 should return data with name "test"!', async () => {
+    const result = await request(app).get("/api/post/1").send();
 
-        expect(result.status).toBe(200);
-        expect(result.body.data[0]?.title).toBe('test');
-    });
+    expect(result.status).toBe(200);
+    expect(result.body.data[0]?.title).toBe("test");
+  });
 
+  it('Request /api/post/?categoryid=1 should return datas with categoryname = "Antikviteter og Kunst"', async () => {
+    const result = await request(app).get("/api/post/1").send();
+
+    expect(result.status).toBe(200);
+    expect(result.body.data[0]?.name).toBe("Antikviteter og Kunst");
+  });
 });
+
