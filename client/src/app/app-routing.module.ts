8ebccfc84b0173033153b92d8874abd1ab28a690
Tormod Nygård <tormodny@stud.ntnu.no>
2021-03-17 11:01:14 +0100

    Issue: Moved home page to separate component (#29)

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,8 +6,11 @@ import { PostListComponent } from './posts/post-list/post-list.component';
 import { UserRegistrationFormComponent } from './users/user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './users/user-login-form/user-login-form.component';
 import { UserProfileComponent } from './users/user-profile/user-profile.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
+
   { path: 'annonse/ny', component: PostFormComponent },
   { path: 'annonse/rediger/:id', component: PostFormComponent },
   { path: 'annonse', component: PostListComponent },
