8a2c2ef31d247a8b5668dff002a5b308c15fb53e
Jonny Ngo Luong <jonnynl@stud.ntnu.no>
2021-03-21 17:52:31 +0100

    Issue: User profile styling (#28)

--- client/src/app/users/user-profile/user-profile.component.ts
+++ client/src/app/users/user-profile/user-profile.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/authentication/auth.service';
+import { Post } from 'src/app/models/post.model';
 import { User } from 'src/app/models/user.model';
+import { PostService } from 'src/app/posts/post.service';
 import { UserService } from '../user.service';
 
 
@@ -11,11 +13,24 @@ import { UserService } from '../user.service';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
-
+  
+  allPosts: Array<Post> = [];
   user: User = new User();
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private postService: PostService, private router: Router) { }
 
   ngOnInit(): void {
     this.user = this.authService.getCurrentUser();
+    this.getPostsByUserId();
+  }
+  placeholder() {
+    console.log(":)");
+  }
+  getPostsByUserId() {
+    // Gets all posts from database, and displays them
+    this.postService.getPostsByUserId(this.user.getUserId).then(posts => {
+      this.allPosts = posts;
+    }).catch(error => {
+      console.log(error);
+    });
   }
 }
