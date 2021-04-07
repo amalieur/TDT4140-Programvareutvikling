f9efd79c967af60f959d10af0891ec688bd8a144
amalieur <amalieur@stud.ntnu.no>
2021-04-07 14:51:13 +0200

    (#20)

--- client/src/app/categories/admin-category/admin-category.component.ts
+++ client/src/app/categories/admin-category/admin-category.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/authentication/auth.service';
+import { PostService } from 'src/app/posts/post.service';
 
 @Component({
   selector: 'app-admin-category',
@@ -6,10 +9,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-category.component.scss']
 })
 export class AdminCategoryComponent implements OnInit {
+  user: new User();
+  name: string = "";
+  statusMessage: string = "";
 
-  constructor() { }
+  constructor(private postService: PostService, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.user = this.authService.getCurrentUser();
+    if (this.user.get)
   }
 
+  /**
+   * Validates the form
+   */
+  checkForm(): boolean {
+    if (this.name == "") {
+      this.setStatusMessage("Brukernavn kan ikke være tom");
+      return false;
+    }
+   
+    this.setStatusMessage("");
+    return true;
+  }
+
+  /**
+   * Publishes and registers the user if given arguments are valid
+   */
+  registerCategory() {
+    if (this.checkForm()) {
+      // Adds user to database and redirects to the homepage afterwards
+      this.postService.addCategory(name).then(status => {
+        console.log("Category was added: " + JSON.stringify(status));
+        loadCategories();
+      }).catch(error => {
+        console.log("Error adding category: " + error);
+      });
+    }
+  }
+
+  /**
+   * Sets the status message for user feedback on form submit
+   */
+  setStatusMessage(message: string) {
+    this.statusMessage = message;
+  }
 }
